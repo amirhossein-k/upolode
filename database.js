ff125814c01b02fee3eaf7a650a4238b901a2a92
amirhossein-k <amirhossein.k787878@gmail.com>
2023-03-12 11:31:36 -0300

    backe

--- database.js
+++ database.js
@@ -2,9 +2,25 @@
 
 const mongoose = require("mongoose");
 
+// module.exports = () => {
+//   mongoose
+//     .connect("mongodb://127.0.0.1:27017/upload-files-database", {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     })
+//     .then(() => console.log("Connected to Mongodb......"));
+// };
+// module.exports = () => {
+//   mongoose
+//     .connect("mongodb://root:nwSFh1bOJAFA2zI3lOhSZ4GP@alfie.iran.liara.ir:33611/my-app?authSource=admin", {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     })
+//     .then(() => console.log("Connected to Mongodb......"));
+// };
 module.exports = () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/upload-files-database", {
+    .connect("mongodb://root:nwSFh1bOJAFA2zI3lOhSZ4GP@alfie.iran.liara.ir:33611/my-app?authSource=admin", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
