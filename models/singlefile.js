d7282ea17e9d14cfd0bda7beca8374bfa7e4f6ec
amirhossein-k <amirhossein.k787878@gmail.com>
2023-03-15 21:18:17 +0330

    f

--- models/singlefile.js
+++ models/singlefile.js
@@ -4,22 +4,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const singleFileSchema = new Schema({
-    fileName: {
-        type: String,
-        required: true
-    },
-    filePath: {
-        type: String,
-        required: true
-    },
-    fileType: {
-        type: String,
-        required: true
-    },
-    fileSize: {
-        type: String,
-        required: true
-    },
+   title: {
+    type:String,
+    required:true
+   },
+   file:{
+    type:Object,
+    required:true
+   }
 },{timestamps:true})
 
 
